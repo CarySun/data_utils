ee3de865b0980d55b5cb0356d14b7b6124ae6391
CarySun <caryjimmy@outlook.com>
2019-01-04 10:15:05 +0800

    change

--- utils.py
+++ utils.py
@@ -1,23 +1,40 @@
 # coding: utf-8
 
+import numpy as np
+import pandas as pd
+import seaborn as sn
 
-def corr_filter(dataset, r=1, headmap=False):
-    
+
+def corr_filter(dataset, r=1.0, headmap=False):
+	"""
+    Feature selection. Through correlation matrix, find out the variables whose correlation exceeds r.
+    Parameters
+    ----------
+    dataset : pandas.Dataset
+    r : sequence of percentile values
+        percentile or percentiles to find score at
+    axis : float
+        correlation coefficient
+    Returns
+    -------
+    corr_feature: list
+       	list of tuples, each tuple contains two related features
+        pandas can transformate it to dataframe
+ 	"""
     corr_mat_bool = ~(dataset.corr() > r)
 
     if headmap:
-        import seaborn
-        seaborn.heatmap(corr_filter)
 
-    import numpy
-    mask = numpy.triu(numpy.ones(corr_mat_bool.shape, dtype=bool))
-    
+        sn.heatmap(corr_filter)
+
+    mask = np.triu(np.ones(corr_mat_bool.shape, dtype=bool))
+
     corr_mat_bool = corr_mat_bool + mask
-    
-    temp = []
+
+    corr_feature = []
     for indexs in corr_mat_bool.index:
         for i in range(len(corr_mat_bool.loc[indexs].values)):
             if (corr_mat_bool.loc[indexs].values[i] == False):
-                temp.append((indexs, corr_mat_bool.index[i]))
-    
-    return temp 
+                corr_feature.append((indexs, corr_mat_bool.index[i]))
+
+    return corr_feature
